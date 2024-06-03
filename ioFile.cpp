d1cc90b7855ff1752995fe9ce2081b6f91f7c343
ZhilPro27 <zhilalkrisna@gmail.com>
2024-06-03 14:36:23 +0700

    Menambahkan conditional statement ketika file tidak dapat dibuka

--- ioFile.cpp
+++ ioFile.cpp
@@ -42,4 +42,7 @@ int main(){
         // Tutup file tersebut setelah selesai
         infile.close();
     }
+    // Jika tidak ditemukan file maka akan menampilkan ini
+    else cout << "Unable to open file";
+    return 0;
 }
