b8870d60eaaffd596ca30d4e4fe2470b98aa1bb1
ZhilPro27 <zhilalkrisna@gmail.com>
2024-06-03 14:18:51 +0700

    Memanggil method untuk membuka file

--- ioFile.cpp
+++ ioFile.cpp
@@ -8,4 +8,8 @@ int main(){
 
     // Membuka file dalam mode menulis.
     ofstream outfile;
+    // Menunjuk ke sebuah nama file
+    outfile.open("contohfile.txt");
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
 }
