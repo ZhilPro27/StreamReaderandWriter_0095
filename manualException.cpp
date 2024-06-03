68dd6c3dc9d7e6c3575c0408e910fe105b96761b
ZhilPro27 <zhilalkrisna@gmail.com>
2024-06-03 15:03:53 +0700

    Membuat exception dengan catch integer dan selain integer

--- manualException.cpp
+++ manualException.cpp
@@ -1,2 +1,20 @@
 #include<iostream>
 using namespace std;
+
+int main() {
+    try {
+        cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        // Throw 0.5; // Melemparkan sebuah integer maka
+        cout << "Pernyataan tidak akan dieksekusi" << endl;
+    }
+    catch (int a) {
+        // Blok ini akan dieksekusi
+        cout << "Pengecualian akan dieksekusi" << endl;
+    }
+    catch (...) {
+        // Jika selain integer maka block ini akan dieksekusi
+        cout << "Default pengecualian dieksekusi" << endl;
+    }
+
+    return 0;
+}
