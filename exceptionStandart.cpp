d613914146fb75c59344915ba89b0b3e67c9bb9b
ZhilPro27 <zhilalkrisna@gmail.com>
2024-06-03 15:16:00 +0700

    Membuat array dan try catch error pada array

--- exceptionStandart.cpp
+++ exceptionStandart.cpp
@@ -4,3 +4,21 @@
 #include<array>
 // Untuk obyek array yang akan kita gunakan
 using namespace std;
+
+int main(){
+    cout << "Awal Program" << endl; // Penanda 1:...
+    try {
+        array<int, 3> data = { 8, 4, 2};
+        // Pesan array integer 3 elemen
+        cout << data.at(5) << endl;
+    }
+    catch (exception& e) {
+        // Penangkap menggunakan obyek exception
+        cout << e.what() << endl;
+        // Akan dieksekusi karna array data hanya memiliki 3 elemen
+    }
+    cout << "Baris Program Yang Terakhir" << endl;
+    // Penanda 2: Bahwa program berjalan tanpa berhenti
+
+    return 0;
+}
