5e13df762216bba38861d3870ef890233c2f0dbc
ZhilPro27 <zhilalkrisna@gmail.com>
2024-06-03 14:27:36 +0700

    Membuat looping untuk menulis ke dalam file

--- ioFile.cpp
+++ ioFile.cpp
@@ -12,4 +12,15 @@ int main(){
     outfile.open("contohfile.txt");
 
     cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    // Unlimited loop untuk menulis
+    while(true) {
+        cout << "- ";
+        // Mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        // Loop akan berhenti jika anda memasukkan memasukan karakter q
+        if (baris == "q") break;
+        // Menulis dan memasukkan nilai dari 'baris' ke dalam file
+        outfile << baris << endl;
+    }
 }
