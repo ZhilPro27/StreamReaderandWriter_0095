f01a11699ffaa3da1ce248e9059ae0c08d8f8d4c
ZhilPro27 <zhilalkrisna@gmail.com>
2024-06-03 14:42:21 +0700

    Membuat objek dari ofstream dan membuka file dalam mode menulis

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -6,4 +6,12 @@ using namespace std;
 int main() {
     string baris;
     string namaFile;
+
+    cout << "Masukkan Nama File: ";
+    cin >> namaFile;
+
+    // Membuka file dalam mode menulis
+    ofstream outfile;
+    // Menunjuk ke sebuah nama file
+    outfile.open(namaFile + ".txt", ios::out);
 }
